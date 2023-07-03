00acdef98d7d5776ac8d18950b7cf437341435bc
jun-ia <jundullah.ilhaq@gmail.com>
2023-07-03 17:05:56 +0700

    add example simple assertion

--- src/test/java/com/jia/basicunittesting/BasicUnitTestingApplicationTests.java
+++ src/test/java/com/jia/basicunittesting/BasicUnitTestingApplicationTests.java
@@ -2,12 +2,28 @@ package com.jia.basicunittesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BasicUnitTestingApplicationTests {
 
     @Test
     void contextLoads() {
+        int num = 3 + 2;
+
+        System.out.println(num);
+
+        assertEquals(num, 5);
+    }
+
+    @Test
+    void newUnitTest() {
+        System.out.println("test add");
+        int add = 5 + 2;
+
+        System.out.println(add);
+
+        assertEquals(7, add, "add equal to 2");
     }
 
 }
